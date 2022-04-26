230b9154abb0c19e16f05c23a4f658e7e0e6eb42
BirTajpuriya <tajpuriyabir00@gmail.com>
2022-04-26 16:38:26 +0545

    little update

--- public_html/login/save_login.php
+++ public_html/login/save_login.php
@@ -2,50 +2,53 @@
 
 include '../../db.php';
 if (isset($_POST['submit'])) {
-    $username=$_POST['name'];
+    $username = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    if($email==''){
-        ?>
-        <script>alert('Please Input Email and Password');</script>
+    if ($email == '') {
+?>
+        <script>
+            alert('Please Input Email and Password');
+        </script>
         <?php
-    }
-    else{
+    } else {
         $email_search = "select * from registration where email='$email' ";
         $query = mysqli_query($conn, $email_search);
         $email_count = mysqli_num_rows($query);
-        if ($email_count>0) {
+        if ($email_count > 0) {
             $email_pass = mysqli_fetch_assoc($query);
-            $login=true;
+            $login = true;
             $db_pass = $email_pass['password'];
-            
+
 
             //comments
             $pass_decode = password_verify($password, $db_pass);
             if ($pass_decode) {
                 session_start();
-            $_SESSION['loggedin']=true;
-            $_SESSION['name']=$username;
+                $_SESSION['loggedin'] = true;
+                $_SESSION['name'] = $username;
                 echo "login success";
                 header('location:../../public_html/pages/index.php');
-               
             } else {
                 // echo " wrong password";
-                ?>
-                <script>alert('invalid Inputs')</script>
-                    
-                <?php
+        ?>
+                <script>
+                    alert('invalid Inputs')
+                </script>
+
+            <?php
                 header('location:./login.php');
             }
         } else {
             // echo "Invalid email";
             ?>
-                <script>alert('invalid')</script>
+            <script>
+                alert('invalid')
+            </script>
 
-            <?php
-        header('location:./login.php');
+<?php
+            header('location:./login.php');
         }
     }
-   
 }
 ?>
